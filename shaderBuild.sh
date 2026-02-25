a75722e7b26267dde99bf23f52b40bdd876e6f8e
Miguel Pafume <leugim.pafume@gmail.com>
2026-02-25 18:19:34 -0300

    unifiex unixbuild file to work on windows

---
+++ shaderBuild.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# %VULKAN_SDK%/slang.exe "file" -profile glsl_450 -target spiriv -o "file".spv -entry computeMain
--- unixBuild.sh
+++ unixBuild.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
 
-mkdir -p build
-cd build 
-cmake -S ../ -B .
-make && make Shaders && ./mage_engine
-cd ..
+build=false;
+run=false;
+windows=false;
+
+while [ ! $# -eq 0 ]
+do  
+    case "$1" in
+        -b | --build)
+            build=true
+            shift
+            ;;
+        -r | --run)
+            run=true
+            shift
+            ;;
+        -w | --windows)
+            windows=true
+            shift
+            ;;
+    esac
+done
+
+if [ $build = true ]
+then
+    mkdir -p build
+
+    if [ $windows = true ]
+    then
+        cmake -B ./build -G "Visual Studio 18 2026"
+        echo "Solution file Written to: ./build/<name>.slnx"
+        # DOESN'T WORK
+        # 'C:\Program Files\Microsoft Visual Studio\18\Community\Common7\IDE\devenv.exe' .\build\template.slnx /Build
+    else
+        cmake -B ./build -G "MinGW Makefiles"
+        make -C ./build
+    fi
+fi
+    
+if [ $run = true ]
+then
+    echo
+    echo "WIP - UNIX RUN"
+fi
